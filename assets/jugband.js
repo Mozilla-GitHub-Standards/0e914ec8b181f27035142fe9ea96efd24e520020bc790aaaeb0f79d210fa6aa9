50fd28eb7e06560da75f192d3ef97d3047f7c1c3
Chuck Harmston <chuck@chuckharmston.com>
2015-07-24 10:24:39 -0600

    A little bit of front-end cleanup.

--- assets/jugband.js
+++ assets/jugband.js
@@ -1,6 +1,7 @@
 (function($) {
 
   var $body, $overlay;
+  var SHOW_NAV = 'show-nav';
 
   $(function() {
     $body = $('body');
@@ -9,9 +10,14 @@
       'class': 'overlay'
     }).insertBefore($body.find('header nav'));
 
-    $body.on('click', '.hamburger, .overlay', function(evt) {
-      evt.preventDefault();
-      $body.toggleClass('show-nav');
+    $body.on('click', '.hamburger', function(evt) {
+      $body.addClass(SHOW_NAV);
+    });
+
+    $body.on('click', 'nav a:not(".active"), .overlay', function(evt) {
+      if ($body.hasClass(SHOW_NAV)) {
+        $body.removeClass(SHOW_NAV);
+      }
     });
   });
 
