add29a3b29c299acac257628565537caff188d16
Chuck Harmston <chuck@chuckharmston.com>
2015-07-29 11:01:24 -0600

    Remove quotes from LinkItem href attribute.

--- components/podio/LinkItem.jsx
+++ components/podio/LinkItem.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default class LinkItem extends React.Component {
   render() {
-    return <li><a href="{this.props.href}" target="_blank">{this.props.label}</a></li>;
+    return <li><a href={this.props.href} target="_blank">{this.props.label}</a></li>;
   }
 }
