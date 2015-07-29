5a97aca1f16229be0b06aada078843c83d5e1aa3
Chuck Harmston <chuck@chuckharmston.com>
2015-07-29 14:54:38 -0600

    Hide footer while throbber is shown, tweak throbber animation and timing.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default class Footer extends React.Component {
   render() {
+    if (this.props.hide) { return false; }
     return (
       <footer>
         <p>Source on <a href="https://github.com/chuckharmston/jugband-react/">Github</a>.</p>
